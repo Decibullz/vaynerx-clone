bec78489ff712c0b3502b66c003c84f3295e5353
Cody Snell <codyjsnell@yahoo.com>
2021-04-19 18:53:17 -0500

    adds first page clone of vaynerX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import './App.css'
+
 import { ReactComponent as Logo } from './logo.svg'
 import { ReactComponent as CLogo } from './colorfulLogo.svg'
+import Page from './components/pages/page.component'
+import bgOne from './imgs/download.png'
+import bgTwo from './imgs/bg2.jpg'
 
 function App() {
   return (
     <div className="App">
       <Logo className="logo" />
-      <CLogo />
+      <div className="app-itemsContainer">
+        <Page
+          logo={<CLogo />}
+          backgroundImg={bgOne}
+          desc={
+            <p>
+              A modern day communications <br /> company.
+            </p>
+          }
+          BtnText=""
+          BtnLink=""
+          textColor={'#fff'}
+          first
+        />
+        <Page
+          backgroundImg={bgTwo}
+          desc={
+            <p>
+              A contemporary global creative and media agency built to drive
+              business outcomes for our partners
+            </p>
+          }
+          textColor={'#fff'}
+        />
+        <Page />
+        <Page last />
+      </div>
     </div>
   )
 }
